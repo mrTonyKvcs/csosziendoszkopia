f6b294e2b6499e3bea70a41bedbfc82b2187887b
tony <attila.kovacs92@gmail.com>
2020-05-25 17:19:12 +0200

    barion in progress

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -6,6 +6,7 @@ use App\Applicant;
 use App\Appointment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
 
 class AppointmentsController extends Controller
 {
@@ -29,11 +30,15 @@ class AppointmentsController extends Controller
             'name' => 'required',
             'phone' => 'required',
             'appointment' => 'required',
+            'email' => 'email'
         ]);
+
 
         $appointment = $this->appointments->find($request->appointment);
 
-        $applicant = Applicant::updateOrCreate($request->except(['_token', 'appointment']));
+        $applicant = Applicant::create($request->only(['name', 'phone', 'appointment', 'email', 'comment']));
+
+        $rs = $this->payment();
 
         $appointment->update(['applicant_id' => $applicant->id]);
 
@@ -57,5 +62,50 @@ class AppointmentsController extends Controller
             return view('appointments.greeting', compact('appointment'));
          }
 
+    }
+
+    public function payment()
+    {
+        require_once app_path('Barion/BarionClient.php');
+        $myPosKey = "b847cf2ff72c4f5e9768595a988c9cbb";
+        $apiVersion = 2;
+
+        // Test environment
+        $environment = \BarionEnvironment::Test;
+        $BC = new \BarionClient($myPosKey, $apiVersion, $environment);
+
+        $item = new \ItemModel();
+        $item->Name = "Online konzultáció";
+        $item->Description = "Online konzultáció jelentkezés";
+        $item->Quantity = 1;
+        $item->Unit = "piece";
+        $item->UnitPrice = 12000;
+        $item->ItemTotal = 12000;
+        $item->SKU = "ITEM-01";
+
+        $trans = new \PaymentTransactionModel();
+        $trans->POSTransactionId = "TRANS-01";
+        $trans->Payee = "attila.kovacs92@gmail.com";
+        $trans->Total = 12000;
+        $trans->Currency = \Currency::HUF;
+        $trans->Comment = "Test transaction containing the product";
+        $trans->AddItem($item);
+
+        $ppr = new \PreparePaymentRequestModel();
+        $ppr->GuestCheckout = true;
+        $ppr->PaymentType = \PaymentType::Immediate;
+        $ppr->FundingSources = array(\FundingSourceType::All);
+        $ppr->PaymentRequestId = "PAYMENT-01";
+        $ppr->PayerHint = "user@example.com";
+        $ppr->Locale = \UILocale::HU;
+        $ppr->OrderNumber = "ORDER-0001";
+        $ppr->Currency = \Currency::HUF;
+        $ppr->RedirectUrl = "http://barion.site/";
+        $ppr->CallbackUrl = "https://demo.csosziendoszkopia.hu/callback";
+        $ppr->AddTransaction($trans);
+
+        $myPayment = $BC->PreparePayment($ppr);
+        //$response = Http::get($myPayment->PaymentRedirectUrl);
+        return redirect($myPayment->PaymentRedirectUrl);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,13 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. groupNow create something great!
 |
 */
+Route::get('/callback', function () {
+    \Log::info($_SERVER);
+});
+
 Route::get('/', [ 'as' => 'pages.index', 'uses' => 'PagesController@index']);
 Route::get('orvosok/{slug}', [ 'as' => 'pages.doctor', 'uses' => 'PagesController@doctor']);
 
