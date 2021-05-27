b255dd62987fa79a0b608fcf44a1d06826042e25
tony <attila.kovacs92@gmail.com>
2021-05-27 16:38:08 +0200

    add barion pixel script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,29 @@
 		<link rel="stylesheet" href="#" id="colors">
 		
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+        <script>
+            // Create BP element on the window
+            window["bp"] = window["bp"] || function () {
+                (window["bp"].q = window["bp"].q || []).push(arguments);
+            };
+            window["bp"].l = 1 * new Date();
+    
+            // Insert a script tag on the top of the head to load bp.js
+            scriptElement = document.createElement("script");
+            firstScript = document.getElementsByTagName("script")[0];
+            scriptElement.async = true;
+            scriptElement.src = 'https://pixel.barion.com/bp.js';
+            firstScript.parentNode.insertBefore(scriptElement, firstScript);
+            window['barion_pixel_id'] = 'BP-E7wx2OLEVn-41';
+
+            // Send init event
+            bp('init', 'addBarionPixelId', window['barion_pixel_id']);
+        </script>
+
+        <noscript>
+            <img height="1" width="1" style="display:none" alt="Barion Pixel" src="https://pixel.barion.com/a.gif?ba_pixel_id='BP-0000000000-00'&ev=contentView&noscript=1">
+        </noscript>
     </head>
     <body>
 	
